b25da2ca3e6ae0c0558cb4a7d061da69042da4f5
basit2023 <babaralibj362@gmail.com>
2024-10-26 17:17:37 +0500

    code updated

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -1,39 +1,39 @@
-import {connect} from "@/dbConfig/dbConfig";
-import { NextRequest, NextResponse } from "next/server";
-import User from "@/models/userModel";
+// import {connect} from "@/dbConfig/dbConfig";
+// import { NextRequest, NextResponse } from "next/server";
+// import User from "@/models/userModel";
 
 
 
-connect()
+// connect()
 
 
-export async function POST(request: NextRequest){
+// export async function POST(request: NextRequest){
 
-    try {
-        const reqBody = await request.json()
-        const {token} = reqBody
-        console.log(token);
+//     try {
+//         const reqBody = await request.json()
+//         const {token} = reqBody
+//         console.log(token);
 
-        const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
+//         const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
 
-        if (!user) {
-            return NextResponse.json({error: "Invalid token"}, {status: 400})
-        }
-        console.log(user);
+//         if (!user) {
+//             return NextResponse.json({error: "Invalid token"}, {status: 400})
+//         }
+//         console.log(user);
 
-        user.isVerfied = true;
-        user.verifyToken = undefined;
-        user.verifyTokenExpiry = undefined;
-        await user.save();
+//         user.isVerfied = true;
+//         user.verifyToken = undefined;
+//         user.verifyTokenExpiry = undefined;
+//         await user.save();
         
-        return NextResponse.json({
-            message: "Email verified successfully",
-            success: true
-        })
+//         return NextResponse.json({
+//             message: "Email verified successfully",
+//             success: true
+//         })
 
 
-    } catch (error:any) {
-        return NextResponse.json({error: error.message}, {status: 500})
-    }
+//     } catch (error:any) {
+//         return NextResponse.json({error: error.message}, {status: 500})
+//     }
 
-}
+// }
